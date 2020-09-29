df611497d80ca14c488530b30e55e5fba947a7c2
VictorMC11 <56057145+VictorMC11@users.noreply.github.com>
2020-09-29 12:23:37 -0600

    Update index.php

--- index.php
+++ index.php
@@ -203,8 +203,8 @@
 					    <label for="name">Nombre completo</label>
 					    <div class="input-group mb-3">
   							<div class="input-group-prepend">
-    						<span class="input-group-text" id="basic-addon1"></span>
-    						<i class="fa fa-user"></i>
+    						<span class="input-group-text" id="basic-addon1"><i class="fa fa-user"></i>
+    						</span>
   							</div>
   						<input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
 						</div>
