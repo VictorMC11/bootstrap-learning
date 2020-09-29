eba98c3c0fedb3925d03eba7e6d1aaffc71f6d62
VictorMC11 <56057145+VictorMC11@users.noreply.github.com>
2020-09-29 12:05:27 -0600

    Update index.php

--- index.php
+++ index.php
@@ -187,23 +187,38 @@
 	</div>
 </div>
 <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Agregar Usuario</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary">Ok</button>
-      </div>
-    </div>
-  </div>
+ 	<div class="modal-dialog modal-lg">
+    	<div class="modal-content">
+     	 	<div class="modal-header">
+       		<h5 class="modal-title" id="staticBackdropLabel">Agregar Usuario</h5>
+       	 	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+         		<span aria-hidden="true">&times;</span>
+       	 	</button>
+     	 	</div>
+     	 	<form>
+	     		<div class="modal-body">
+	       			<div class="form-group">
+					    <label for="name">Nombre completo</label>
+					    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+					    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+					  </div>
+					  <div class="form-group">
+					    <label for="exampleInputPassword1">Password</label>
+					    <input type="password" class="form-control" id="exampleInputPassword1">
+					  </div>
+					  <div class="form-group form-check">
+					    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+					    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+					  </div>
+ 					<button type="submit" class="btn btn-primary">Submit</button>
+	      		</div>
+	     		<div class="modal-footer">
+	       			<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+	      			<button type="submit" class="btn btn-primary">Ok</button>
+	     		</div>
+    		</form>	
+    	</div>
+ 	 </div>
 </div>
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 	
