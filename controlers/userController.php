01228aba051d57b473adf670e89b0d973ef0f61b
VictorMC11 <56057145+VictorMC11@users.noreply.github.com>
2020-10-01 13:04:18 -0600

    Update userController.php

--- controlers/userController.php
+++ controlers/userController.php
@@ -6,7 +6,7 @@
     Class UserController{
     	function get(){
     		$con = connect();
-    		if($con->connect_error){
+    		if(!$con->connect_error){
 
     			$query = "select * from users";
     			$prepared_query = $con->prepare($query);
@@ -14,11 +14,13 @@
 
     			$results = $prepared_query->get_result();
     			$users = $results->fetch_all(MYSQLI_ASSOC);
-
     			if($users){
     				return $users;
-    			}
-    		}
+    			}else{
+                    return array();
+                }
+    		}else
+                return array();
     	}
     }
     
