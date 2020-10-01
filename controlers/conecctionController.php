76f34def6a4400cefbf90e744cde182b4a27b6a6
VictorMC11 <56057145+VictorMC11@users.noreply.github.com>
2020-10-01 11:45:29 -0600

    Update conecctionController.php

--- controlers/conecctionController.php
+++ controlers/conecctionController.php
@@ -4,4 +4,13 @@
     define("USER", "root");
     define("PASS", "");
     define("DBNM", "crud");
+
+    function connect(){
+    	$con = new mysqli(HOST, USER, PASS, DBNM);
+    	if($con){
+    		return $con;
+    	}else{
+    		return null;
+    	}
+    }
 ?>
