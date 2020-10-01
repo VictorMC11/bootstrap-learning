7ed3abe02431bee606d1aea15fff20afbc9f392b
VictorMC11 <56057145+VictorMC11@users.noreply.github.com>
2020-10-01 12:16:35 -0600

    Create userController.php

---
+++ controlers/userController.php
@@ -0,0 +1,27 @@
+<?php 
+    
+    include_once "config.php";
+    include_once "connectionController.php";
+
+    Class userController{
+    	function get(){
+    		$con = connect();
+    		if($con->connect_error){
+
+    			$query = "SELECT * FROM users";
+    			$prepared_query = $con->prepare($query);
+    			$prepared_query->execute();
+
+    			$status = $prepared_query->get_result();
+    			$users = $results->fetch_all(MYSQL_ASSOC);
+
+    			if($users){
+    				return $users;
+    			}
+
+    			return $users;
+    		}
+    	}
+    }
+    
+?>
