58abc4bb587e736dd040ec251d38d99fb90b1c89
VictorMC11 <56057145+VictorMC11@users.noreply.github.com>
2020-10-01 11:38:29 -0600

    Update config.php

--- controlers/config.php
+++ controlers/config.php
@@ -1,7 +1,12 @@
 <?php 
-
+    
+    //Mostrar errores
     init_set('display_errors',1);
     init_set('display_startup_errors',1);
     error_reporting(E_ALL);
 
+    //Iniciar sesión
+    if(!isset($_SESSION)){
+        session_start();
+    }
 ?>
