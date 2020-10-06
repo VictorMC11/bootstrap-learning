25c726e9a65e262597df978184606ca5c348e659
VictorMC11 <56057145+VictorMC11@users.noreply.github.com>
2020-10-06 12:05:18 -0600

    Update userController.php

--- controlers/userController.php
+++ controlers/userController.php
@@ -3,6 +3,21 @@
     include_once "config.php";
     include_once "connectionController.php";
 
+    if(isset($_POST) && isset($_POST['action'])){
+        $userController = new UserController();     
+        switch ($_POST['action']) {
+            case 'store':
+
+                    $name = strip_tags($_POST['name']);
+                    $email = strip_tags($_POST['email']);
+                    $pass = strip_tags($_POST['pass1']);
+
+                    $userController->store($name,$email,$pass);
+
+                break;
+        }
+    }
+
     Class UserController{
         
         function get(){
@@ -27,11 +42,11 @@
             $con = connect();
             if(!$con->connect_error){
 
-                if($name!="" && $maill!="" && $password!=""){
-                    $query= "INSERT INTO users (name, email, password) VALUES (?,?,?)";
+                if($name!="" && $email!="" && $password!=""){
+                    $query= "insert into users (nombre, correo, password) values (?,?,?)";
                     $prepared_query = $con->prepare($query);
                     $prepared_query->bind_param('sss',$name,$email,$password);
-                    if($prepared_query>execute()){
+                    if($prepared_query->execute()){
                         header('Location: ' . $_SERVER['HTTP_REFERER']);
                     }
                 }else{
